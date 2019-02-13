56f21a74a43ef2bccb4268b5d6f59ef070ef1c28
Vinayak Sinha <vinnusinha@gmail.com>
2019-02-13 21:48:39 +0530

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigid;
+    private float _speed = 3f;
+    private Animator _anim;
+
+    private SpriteRenderer _playerSprite;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+        _anim = GetComponentInChildren<Animator>();
+        _playerSprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+
+        float move = Input.GetAxisRaw("Horizontal");
+        bool isRunning;
+        if(move == 0)
+        {
+            isRunning = false;
+        }
+        else
+        {
+            isRunning = true;
+        }
+        if (move > 0)
+        {
+            Flip(true);
+        }
+        else if (move < 0)
+        {
+            Flip(false);
+        }
+
+        _rigid.velocity = new Vector2(move * _speed, _rigid.velocity.y);
+
+        _anim.SetBool("isRunning", isRunning);
+    }
+
+    void Flip(bool FaceRight)
+    {
+        if (FaceRight == true)
+        {
+            _playerSprite.flipX = false;
+            
+        }
+        else if (FaceRight == false)
+        {
+            _playerSprite.flipX = true;
+            
+            }
+    }
+}
